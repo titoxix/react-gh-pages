2ceb422d8a3aeb30ba78d9d6c15fe40b73f61f3f
Matias Martinez <mat.360z@gmail.com>
2019-03-18 21:16:06 -0300

    Updates

---
+++ precache-manifest.0efea7f07626ce2b392e2d1a241c3cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "b6752562ac591b48af8d",
+    "url": "/react-gh-pages/static/js/main.a3ba8666.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react-gh-pages/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "b6752562ac591b48af8d",
+    "url": "/react-gh-pages/static/css/main.60ff12a0.chunk.css"
+  },
+  {
+    "revision": "2600b6f1f32d19d339afe6cb4a0edbd7",
+    "url": "/react-gh-pages/index.html"
+  }
+];
